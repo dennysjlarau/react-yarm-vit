6b7b236a8b9f47e43833da53f6c94975846be53e
Dennys Javier Lara Uquillas <dennysjlarau@gmail.com>
2022-10-20 09:11:29 -0500

    crear nuevo componente

---
+++ src/FirstApp.jsx
@@ -0,0 +1,6 @@
+export const FirstApp = () => {
+  return (
+    <h2>Dennys Javier Lara Uquillas</h2>
+  )
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {HelloWordApp} from './HelloWordApp'
+import {FirstApp} from './FirstApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWordApp />
+    <FirstApp />
   </React.StrictMode>
 );

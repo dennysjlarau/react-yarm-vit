0c3b106edc722f5e83671f2de80b5879ef6b35ea
Dennys Javier Lara Uquillas <dennysjlarau@gmail.com>
2022-10-20 11:56:52 -0500

    implementación del usestate y hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
+import {useState} from 'react';
 
 export const CounterApp = ({value}) => {
+
+  const [counter, setCounter] = useState(value);
+
   const handleAdd = (event, value) => { 
-    console.log(event);
-    value = value + 1;
-    console.log(value);
+    setCounter(counter + 1);
+    // setCounter((c) => c +1);
   }
   
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> { value } </h2>
+      <h2> { counter } </h2>
 
       <button onClick={ (event) => handleAdd(event, value) }>
         +1

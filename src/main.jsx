6b7349694b9af0c2491c440597b1d1fbcfbd64e1
Dennys Javier Lara Uquillas <dennysjlarau@gmail.com>
2022-10-20 09:58:22 -0500

    creación de estilos globales

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import {HelloWordApp} from './HelloWordApp'
 import {FirstApp} from './FirstApp'
 
+import './styles-global.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FirstApp />

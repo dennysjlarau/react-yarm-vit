623c762787f0ab12c9deabf2c14ca477230b6acd
Dennys Javier Lara Uquillas <dennysjlarau@gmail.com>
2022-10-20 09:22:09 -0500

    implementación de fragmentos en firstapp

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,6 +1,19 @@
+// import { Fragment } from "react"
+
+// export const FirstApp = () => {
+//   return (
+//     <Fragment>
+//       <h2>Dennys Javier Lara Uquillas</h2>
+//       <p>Este es un ejemplo de párrafo</p>
+//     </Fragment>
+//   )
+// }
+
 export const FirstApp = () => {
   return (
-    <h2>Dennys Javier Lara Uquillas</h2>
+    <>
+      <h2>Dennys Javier Lara Uquillas</h2>
+      <p>Este es un ejemplo de párrafo</p>
+    </>
   )
 }
-

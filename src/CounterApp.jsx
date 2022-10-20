296bd9e328614f222e8ec251d3ad97afcd4be3b7
Dennys Javier Lara Uquillas <dennysjlarau@gmail.com>
2022-10-20 11:37:31 -0500

    implementación del evento click

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,20 @@
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({value}) => {
+  const handleAdd = (event, value) => { 
+    console.log(event);
+    value = value + 1;
+    console.log(value);
+  }
+  
   return (
     <>
       <h1>CounterApp</h1>
       <h2> { value } </h2>
+
+      <button onClick={ (event) => handleAdd(event, value) }>
+        +1
+      </button>
     </>
   )
 }

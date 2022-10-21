5e3ac05cc0d196621e461d7be08093bb7c46813c
Dennys Javier Lara Uquillas <dennysjlarau@gmail.com>
2022-10-21 17:37:26 -0500

    añadir reac testing library para probar dom

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp value={7}/>
+    <FirstApp />
   </React.StrictMode>
 );
